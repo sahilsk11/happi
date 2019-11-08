f0123d7fb9353448b7c8f56cb7aa3c362752cb25
Sahil Kapur <sahilkapur.a@gmail.com>
2019-11-08 01:43:30 -0500

    add second player

--- prototype.py
+++ prototype.py
@@ -2,55 +2,57 @@ import click
 import subprocess
 from time import sleep
 
-player = {
-	"body":[" _", "(_)", "\|__->", " |", "/ \\"],
-	"attack": (2, "\|__----->")
-}
-
 screen_width = int(subprocess.check_output(["tput", "cols"])[:3])
 player_width = 10
 
-def print_player(location, attack=False):
-	for _ in range(0, location["y"]):
+player1 = {
+	"body":[" _", "(_)", "\|__->", " |", "/ \\"],
+	"attack": (2, "\|__----->"),
+	"damage": 0.0,
+	"bounds": [[0, 0], [6, 5]]
+}
+
+player2 = {
+	"body":["_ ", "(_)", "<-__|/", "| ", "/ \\"],
+	"attack": (2, "<-----__|/"),
+	"damage": 0.0,
+	"bounds": [[screen_width-6, 0], [screen_width, 5]]
+}
+
+def print_screen():
+	subprocess.run('clear')	
+	for _ in range(0, 6):
 		print()
-	for i in range(0, len(player["body"])):
-		for _ in range(0, location["x"]):
+	# print players
+	for i in range(0, 5):
+		for _ in range(0, player1["bounds"][0][0]):
 			print(" ", end="")
-		if attack and i == player["attack"][0]:
-			print(player["attack"][1])
-		else:
-			print(player["body"][i])
-	# print ground
-	for _ in range(0, 10 - location["y"]):
-		print()
+		print(player1["body"][i], end="")
+		for _ in range(len(player1["body"][i]) + player1["bounds"][0][0], screen_width - len(player2["body"][i])):
+			print(" ", end="")
+		print(player2["body"][i], end="")
 	for _ in range(0, screen_width):
 		print("=", end="")
 	print("DAMAGE: 0.0%")
-
-
-subprocess.run('clear')
-player1_location = {"x": 0, "y": 10}
-print_player(player1_location)
+print_screen()	
 while (True):
 	c = click.getchar()
 	if c == '\x1b[D':
-		if (player1_location["x"] > 0):
-			player1_location["x"] -= 1
+		if (player1["bounds"][0][0] > 0):
+			player1["bounds"][0][0] -= 1
+			player2["bounds"][1][0] -= 1
 	elif c == '\x1b[C':
-		if (player1_location["x"] < screen_width - player_width):
-			player1_location["x"] += 1
+		if (player1["bounds"][1][0] < screen_width and player1["bounds"][1][0] < player2["bounds"][0][0]):
+			player1["bounds"][0][0] += 1
+			player1["bounds"][1][0] += 1
 	elif c == '\x1b[A':
 		subprocess.run('clear')
-		player1_location["y"] -= 6
-		print_player(player1_location, True)
-		sleep(0.24)
-		player1_location["y"] += 6
 	elif c == ' ':
 		subprocess.run('clear')
-		print_player(player1_location, True)
+	#	print_player(player1_location, True)
 		sleep(0.2)
 	elif c == 'q':
 		exit(1)
 
 	subprocess.run('clear')	
-	print_player(player1_location)
+	print_screen()
